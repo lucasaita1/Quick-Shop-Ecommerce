c30b37aad5c131af6c5f18f4278c9c94f931756a
lucas <lucasaita4000@gmail.com>
2025-08-25 22:15:48 -0300

    feat: adiciona a anotacao de @EnableFeingClients para habilitar o OpenFeing

--- src/main/java/dev/lucas/ecommerce/basketservice/BasketserviceApplication.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/BasketserviceApplication.java
@@ -2,12 +2,13 @@ package dev.lucas.ecommerce.basketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "client")
 public class BasketserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasketserviceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BasketserviceApplication.class, args);
+    }
 }
