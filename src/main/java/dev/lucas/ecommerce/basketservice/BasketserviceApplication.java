5ba0180411b54d56ca42770cea79e2ae97770527
lucas <lucasaita4000@gmail.com>
2025-08-26 00:41:17 -0300

    feat: adiciona a anotacao @EnableCaching para habilitar nosso application a trabalhar com o cache do Redis

--- src/main/java/dev/lucas/ecommerce/basketservice/BasketserviceApplication.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/BasketserviceApplication.java
@@ -2,9 +2,11 @@ package dev.lucas.ecommerce.basketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
 @EnableFeignClients(basePackages = "dev.lucas.ecommerce.basketservice.client")
 public class BasketserviceApplication {
 
