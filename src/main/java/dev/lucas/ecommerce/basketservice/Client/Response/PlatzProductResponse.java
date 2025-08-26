665325326342a38acaf5d1616686871e6ffcd5cb
lucas <lucasaita4000@gmail.com>
2025-08-26 00:37:29 -0300

    feat: adiciona a implementacao do Serializable para redis guardar o objeto no cache

--- src/main/java/dev/lucas/ecommerce/basketservice/Client/Response/PlatzProductResponse.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Client/Response/PlatzProductResponse.java
@@ -1,6 +1,7 @@
 package dev.lucas.ecommerce.basketservice.Client.Response;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
-public record PlatzProductResponse(Long id, String title, String description, BigDecimal price) {
+public record PlatzProductResponse(Long id, String title, String description, BigDecimal price) implements Serializable {
 }
