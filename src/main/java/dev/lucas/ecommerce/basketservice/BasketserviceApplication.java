c548d7fbdc04d04dc8506aab8afd1486497746c4
lucas <lucasaita4000@gmail.com>
2025-08-25 23:21:39 -0300

    fix: corrige erro na anotacao de basePackeges

--- src/main/java/dev/lucas/ecommerce/basketservice/BasketserviceApplication.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/BasketserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages = "client")
+@EnableFeignClients(basePackages = "dev.lucas.ecommerce.basketservice.client")
 public class BasketserviceApplication {
 
     public static void main(String[] args) {
