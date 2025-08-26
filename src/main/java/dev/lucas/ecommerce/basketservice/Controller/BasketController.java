ee24d1096ac1c6ef8406dcfa8387700b37f98958
lucas <lucasaita4000@gmail.com>
2025-08-26 03:35:58 -0300

    feat: adiciona a BasketController com os endpoints e rotas funcionais

---
+++ src/main/java/dev/lucas/ecommerce/basketservice/Controller/BasketController.java
@@ -0,0 +1,33 @@
+package dev.lucas.ecommerce.basketservice.Controller;
+
+import dev.lucas.ecommerce.basketservice.Controller.Request.BasketRequest;
+import dev.lucas.ecommerce.basketservice.Entity.Basket;
+import dev.lucas.ecommerce.basketservice.Service.BasketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/basket")
+public class BasketController {
+
+    private final BasketService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Basket> basketById(@PathVariable String id) {
+        return service.getBasketById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Basket> createBasket(@RequestBody BasketRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createBasket(request));
+    }
+
+
+}
