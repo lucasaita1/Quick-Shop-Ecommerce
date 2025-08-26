b5c32f1d9463153e5d5fcba8835a48eea7964df7
lucas <lucasaita4000@gmail.com>
2025-08-25 23:22:30 -0300

    feat: adiciona uma string para vermos a descricao do objeto no método GET

--- src/main/java/dev/lucas/ecommerce/basketservice/Client/Response/PlatzProductResponse.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Client/Response/PlatzProductResponse.java
@@ -2,5 +2,5 @@ package dev.lucas.ecommerce.basketservice.Client.Response;
 
 import java.math.BigDecimal;
 
-public record PlatzProductResponse(Long id, String title, BigDecimal price) {
+public record PlatzProductResponse(Long id, String title, String description, BigDecimal price) {
 }
