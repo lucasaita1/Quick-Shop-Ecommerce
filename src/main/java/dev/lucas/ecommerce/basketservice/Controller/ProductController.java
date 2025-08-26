858c4db13029ccf88cd07fc4194819758a5e7c40
lucas <lucasaita4000@gmail.com>
2025-08-25 22:16:45 -0300

    feat: adiciona os endpoints no ProductController para acessarmos a API externa

---
+++ src/main/java/dev/lucas/ecommerce/basketservice/Controller/ProductController.java
@@ -0,0 +1,31 @@
+package dev.lucas.ecommerce.basketservice.Controller;
+
+import dev.lucas.ecommerce.basketservice.Client.Response.PlatzProductResponse;
+import dev.lucas.ecommerce.basketservice.Service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+
+  private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<PlatzProductResponse>> getAllProduct() {
+        return ResponseEntity.ok(productService.getAllProduct());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PlatzProductResponse> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+}
