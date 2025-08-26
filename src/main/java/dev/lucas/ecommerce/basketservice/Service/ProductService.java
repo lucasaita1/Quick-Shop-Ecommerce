8aadc5babde113df58e71b29a426ce868bb069e1
lucas <lucasaita4000@gmail.com>
2025-08-26 00:39:53 -0300

    feat: configura o Redis para guardar o retorno dos métodos do Service no cache

--- src/main/java/dev/lucas/ecommerce/basketservice/Service/ProductService.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Service/ProductService.java
@@ -4,21 +4,28 @@ package dev.lucas.ecommerce.basketservice.Service;
 import dev.lucas.ecommerce.basketservice.Client.PlatzStoreClient;
 import dev.lucas.ecommerce.basketservice.Client.Response.PlatzProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatzStoreClient platzStoreClient;
 
+    @Cacheable(value = "products")
     public List<PlatzProductResponse> getAllProduct(){
+        log.info("Getting all products");
        return platzStoreClient.getAllProduct();
     }
 
-    public PlatzProductResponse getProductById (Long id){
-        return platzStoreClient.getProductById(id);
+    @Cacheable(value = "poduct", key = "#productId")
+    public PlatzProductResponse getProductById (Long productId){
+        log.info("Getting product with id: {}", productId);
+        return platzStoreClient.getProductById(productId);
     }
 }
