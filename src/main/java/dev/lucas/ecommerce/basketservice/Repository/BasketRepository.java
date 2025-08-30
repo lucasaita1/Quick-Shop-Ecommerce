f6037830a83a6311181ef2daee9384b1afc6f933
lucas <lucasaita4000@gmail.com>
2025-08-30 20:30:42 -0300

    feat: adiciona um novo método de consultar baskets por clients

--- src/main/java/dev/lucas/ecommerce/basketservice/Repository/BasketRepository.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Repository/BasketRepository.java
@@ -5,13 +5,13 @@ import dev.lucas.ecommerce.basketservice.Enum.Status;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface BasketRepository extends MongoRepository <Basket, String> {
+public interface BasketRepository extends MongoRepository<Basket, String> {
 
-    Optional<Basket> findByClientAndStatus(Long client, Status status);
+    Optional<Basket> findByClientAndStatus(String client, Status status);
 
-
+    List<Basket> findByClient(String client);
 }
-
--- src/main/java/dev/lucas/ecommerce/basketservice/Service/BasketService.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Service/BasketService.java
@@ -4,16 +4,19 @@ import dev.lucas.ecommerce.basketservice.Client.Response.PlatzProductResponse;
 import dev.lucas.ecommerce.basketservice.Controller.Request.BasketRequest;
 import dev.lucas.ecommerce.basketservice.Controller.Request.PaymentRequest;
 import dev.lucas.ecommerce.basketservice.Entity.Basket;
+import dev.lucas.ecommerce.basketservice.Entity.Client;
 import dev.lucas.ecommerce.basketservice.Entity.Product;
 import dev.lucas.ecommerce.basketservice.Enum.Status;
 import dev.lucas.ecommerce.basketservice.Exceptions.BusinessException;
 import dev.lucas.ecommerce.basketservice.Exceptions.DataNotFoundException;
 import dev.lucas.ecommerce.basketservice.Repository.BasketRepository;
+import dev.lucas.ecommerce.basketservice.Repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -22,6 +25,7 @@ public class BasketService {
 
     private final BasketRepository basketRepository;
     private final ProductService productService;
+    private final ClientRepository clientRepository;
 
     public Basket getBasketById (String id){
         return basketRepository.findById(id).orElseThrow(()-> new DataNotFoundException("Basket not found"));
@@ -59,6 +63,14 @@ public class BasketService {
         savedbasket.setPaymentMethod(paymentRequest.getPaymentMethod());
         savedbasket.setStatus(Status.CLOSE);
         return basketRepository.save(savedbasket);
+    }
+
+    public List<Basket> getBasketFromClient (String clientId){
+        Optional<Client> client = clientRepository.findById(clientId);
+        if (client.isEmpty()){
+            throw new DataNotFoundException("Client not found with id: " + clientId);
+        }
+        return basketRepository.findByClient(clientId);
     }
 
     private List<Product> getProducts(BasketRequest basketRequest) {
