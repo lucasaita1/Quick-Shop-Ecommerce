1df060b61f857408164e0830430d1bc9aed495b8
lucas <lucasaita4000@gmail.com>
2025-09-01 03:20:43 -0300

    feat: adiciona o método findByEmail para o AuthService consumir e fazer a validacao do login

--- src/main/java/dev/lucas/ecommerce/basketservice/Repository/ClientRepository.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Repository/ClientRepository.java
@@ -1,9 +1,14 @@
 package dev.lucas.ecommerce.basketservice.Repository;
 
 import dev.lucas.ecommerce.basketservice.Entity.Client;
+import org.springframework.data.domain.Example;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository extends MongoRepository<Client, String> {
+
+    Optional<Client> findByEmail (String email);
 }
