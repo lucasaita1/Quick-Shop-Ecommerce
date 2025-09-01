7995c7b952a00ac5562de83bc6cd707e62a99233
lucas <lucasaita4000@gmail.com>
2025-09-01 03:11:29 -0300

    feat: adiciona a criptografia no metodo de salvar novos clients e também adiciona exceptions caso o usuario tente criar novos registros usando um email e cpfs repetidos

--- src/main/java/dev/lucas/ecommerce/basketservice/Service/ClientService.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Service/ClientService.java
@@ -1,18 +1,35 @@
 package dev.lucas.ecommerce.basketservice.Service;
 
 import dev.lucas.ecommerce.basketservice.Entity.Client;
+import dev.lucas.ecommerce.basketservice.Exceptions.UserCreateArgumentExeption;
 import dev.lucas.ecommerce.basketservice.Repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
 
+
     private final ClientRepository clientRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Client creatClient (Client client){
-        return clientRepository.save(client);
+        try {
+            String password = client.getPassword();
+            client.setPassword(passwordEncoder.encode(password));
+            return clientRepository.save(client);
+
+        } catch (Exception e) {
+            if (e.getMessage().contains("email")) {
+                throw new UserCreateArgumentExeption("Email already registered");
+            } else if (e.getMessage().contains("cpf")) {
+                throw new UserCreateArgumentExeption("CPF already registered");
+            }
+            throw new UserCreateArgumentExeption("Duplicate key error");
+        }
+        }
     }
 
-}
