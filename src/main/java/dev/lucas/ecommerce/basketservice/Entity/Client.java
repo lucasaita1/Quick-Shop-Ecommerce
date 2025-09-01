f5d2645654b668035e6b77b629f32b691a1df1e6
lucas <lucasaita4000@gmail.com>
2025-09-01 03:13:32 -0300

    feat: adiciona as anotacoes de @Indexed(unique = true) para garantir que nao salve novos clients no banco de dados com as credencias repetidas

--- src/main/java/dev/lucas/ecommerce/basketservice/Entity/Client.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Entity/Client.java
@@ -2,7 +2,13 @@ package dev.lucas.ecommerce.basketservice.Entity;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Getter
@@ -11,18 +17,49 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @Builder
 @Document (collection = "clients")
-public class Client {
+public class Client implements UserDetails {
 
     @Id
     private String id;
 
     private String name;
 
+    @Indexed(unique = true)
     private String email;
 
+    @Indexed(unique = true)
     private String cpf;
 
     private String password;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
