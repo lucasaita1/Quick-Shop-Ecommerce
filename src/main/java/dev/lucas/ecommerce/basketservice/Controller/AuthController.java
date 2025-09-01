eb1957801f858980a38d2c0da57641f243cbf5c4
lucas <lucasaita4000@gmail.com>
2025-09-01 03:14:56 -0300

    feat: cria o endpoint ou rota de login para receber o token de autenticacao para as outras rotas.

--- src/main/java/dev/lucas/ecommerce/basketservice/Controller/AuthController.java
+++ src/main/java/dev/lucas/ecommerce/basketservice/Controller/AuthController.java
@@ -1,6 +1,8 @@
 package dev.lucas.ecommerce.basketservice.Controller;
 
+import dev.lucas.ecommerce.basketservice.Config.TokenComponent;
 import dev.lucas.ecommerce.basketservice.Controller.Request.ClientRequest;
+import dev.lucas.ecommerce.basketservice.Controller.Request.LoginRequest;
 import dev.lucas.ecommerce.basketservice.Controller.Response.ClientResponse;
 import dev.lucas.ecommerce.basketservice.Entity.Client;
 import dev.lucas.ecommerce.basketservice.Mapper.ClientMapper;
@@ -8,6 +10,12 @@ import dev.lucas.ecommerce.basketservice.Service.ClientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final ClientService service;
+    private final AuthenticationManager authenticationManager;
+    private final TokenComponent tokenComponent;
 
     @PostMapping ("/register")
     public ResponseEntity<ClientResponse> createClient (@RequestBody ClientRequest request){
@@ -26,5 +36,21 @@ public class AuthController {
         Client newClient = service.creatClient(client);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ClientMapper.toResponse(client));
+    }
+
+    @PostMapping ("/login")
+    public ResponseEntity<String> login (@RequestBody LoginRequest loginRequest){
+        try {
+            UsernamePasswordAuthenticationToken userAndPassword = new UsernamePasswordAuthenticationToken(loginRequest.email(), loginRequest.password());
+            Authentication authentication = authenticationManager.authenticate(userAndPassword);
+
+            Client client = (Client) authentication.getPrincipal();
+            String token = tokenComponent.generateToken(client);
+
+            return ResponseEntity.ok(token);
+        } catch (BadCredentialsException exception){
+            throw new UsernameNotFoundException("User or password incorrects");
+        }
+
     }
 }
